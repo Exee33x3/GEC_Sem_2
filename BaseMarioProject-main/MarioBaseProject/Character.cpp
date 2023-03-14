2e24e379d9e2bf9688af533a1b4fd89f377e3492
Exee33x3 <115216011+Exee33x3@users.noreply.github.com>
2023-03-14 14:39:24 +0000

    e

---
+++ BaseMarioProject-main/MarioBaseProject/Character.cpp
@@ -0,0 +1,13 @@
+#include "Character.h"
+#include "Texture2D.h"
+
+Character::Texture2D() 
+{
+
+}
+Character::
+Character::
+Character::
+Character::
+Character::
+Character::
---
+++ BaseMarioProject-main/MarioBaseProject/Character.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef _CHARACTER_H
+#define _CHARACTER_H
+
+#include <SDL.h>
+#include <iostream>
+#include "Commons.h"
+#include <string>
+using namespace std;
+
+class Texture2D;
+
+class Character
+{
+	
+
+protected:
+	SDL_Renderer* m_renderer;
+	Vector2D m_position;
+	Texture2D* m_texture;
+	
+
+public:
+	Character(SDL_Renderer* renderer, string imagePath, Vector2D start_position);
+	~Character();
+
+	virtual void Render();
+	virtual void Update(float deltaTime, SDL_Event e);
+	void SetPosition(Vector2D new_position);
+	Vector2D GetPosition();
+
+
+
+};
+
+
+
+#endif
